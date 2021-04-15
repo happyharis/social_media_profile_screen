9e4fabbe4d9f8f87487ddace18efb0e159b6277a
Haris Samingan <muhamad_haris@hotmail.com>
2021-04-15 11:26:44 +0800

    Add top header and pixel perfect package

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pixel_perfect/pixel_perfect.dart';
 
 const iconsPath = 'assets/icons/';
 const imagePath = 'assets/images/';
@@ -19,6 +20,46 @@ class MyApp extends StatelessWidget {
 class ProfileScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return PixelPerfect(
+      assetPath: imagePath + 'profile_light.jpeg',
+      child: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 9.0, horizontal: 15),
+          child: Scaffold(
+            body: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  children: [
+                    Text(
+                      'pieroborgo',
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.w700,
+                        letterSpacing: -1,
+                      ),
+                    ),
+                    Spacer(),
+                    IconButton(
+                      onPressed: () {},
+                      icon: Image.asset(iconsPath + 'new-video.png'),
+                      constraints: BoxConstraints.tight(Size(21.5, 21.5)),
+                      padding: EdgeInsets.zero,
+                    ),
+                    SizedBox(width: 30.5),
+                    IconButton(
+                      onPressed: () {},
+                      icon: Image.asset(iconsPath + 'menu.png'),
+                      constraints: BoxConstraints.tight(Size(21.5, 21.5)),
+                      padding: EdgeInsets.zero,
+                    ),
+                  ],
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
